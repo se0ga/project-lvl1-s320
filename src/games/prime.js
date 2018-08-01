a8def47c727387ab93fd6776c0a4918607d51f60
sergeiiak <sergeiiak@yandex-team.ru>
2018-08-01 22:47:42 +0100

    add game brain-prime

---
+++ src/games/prime.js
@@ -0,0 +1,29 @@
+import playGame from '../game';
+import rand from '../utils';
+
+const description = 'Is this number prime?';
+const minNumber = 0;
+const maxNumber = 100;
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export default () => {
+  const getQuestionAndAnswer = () => {
+    const number = rand(minNumber, maxNumber);
+    const question = `${number}`;
+    const result = isPrime(number);
+    const answer = result ? 'yes' : 'no';
+    return [question, answer];
+  };
+  playGame(description, getQuestionAndAnswer);
+};
